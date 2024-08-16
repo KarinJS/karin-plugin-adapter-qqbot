176054c9b39e819962c3c01f536d305831a63850
时瑾 <shijinn520@gmail.com>
2024-08-16 23:54:12 +0800

    fix: build error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import './core/index'
+import Jimp from 'jimp'
 import { logger, common } from 'node-karin'
 import { basename, dirPath } from './utils/dir'
-import Jimp from 'jimp'
 
 const pkg = common.readJson(dirPath + '/package.json')
-
 logger.info(`${logger.violet(`[插件:${pkg.version}]`)} ${logger.green(basename)} 初始化完成~`)
+
 export default Jimp
