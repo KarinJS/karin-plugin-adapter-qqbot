577e5148b6f729c5b2e98d954bb19ef853547648
时瑾 <74231782+sj817@users.noreply.github.com>
2025-05-10 12:45:09 +0800

    fix: build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,6 @@
-import fs from 'node:fs'
-import { URL } from 'node:url'
 import { defineConfig } from 'tsup'
-import type { Options } from 'tsup'
 
-const pkg = JSON.parse(fs.readFileSync(new URL('package.json', import.meta.url), 'utf-8'))
-/**
- * @description `tsup` configuration options
- */
-export const options: Options = {
+export default defineConfig({
   entry: ['src/index.ts'], // 入口文件
   format: ['esm'], // 输出格式
   target: 'node16', // 目标环境
@@ -18,9 +11,7 @@ export const options: Options = {
   outDir: 'dist', // 输出目录
   treeshake: false, // 树摇优化
   minify: false, // 压缩代码
-  external: [...Object.keys(pkg.dependencies), 'node-karin'],
-  ignoreWatch: ['src/modules'],
+  external: ['node-karin'],
+  ignoreWatch: [],
   shims: true,
-}
-
-export default defineConfig(options)
+})
