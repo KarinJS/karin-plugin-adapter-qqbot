357eb82ec17ccf53b63599065c5cba7b552e797d
瑜笙 <2624367622@qq.com>
2026-08-17 22:58:52 +0800

    fix: 修复url拼接错误的问题

--- src/core/internal/axios.ts
+++ src/core/internal/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from 'node-karin/axios'
 import { log } from '@/utils/logger'
 import { formatOpenAPIError } from '@/core/api/error'
 import { getUserAgent } from '@/utils/user-agent'
-import path from 'path'
+import { URL } from 'url'
 import { QQAPIBASEURL } from '@/utils/common'
 
 /**
@@ -32,7 +32,7 @@ export const getAccessToken = async (
 ): Promise<{ accessToken: string; expiresIn: number }> => {
   let res
   try {
-    const tokenurl = path.join(QQAPIBASEURL, '/app/getAppAccessToken')
+    const tokenurl = new URL('/app/getAppAccessToken', QQAPIBASEURL)
     res = await axios.post<AccessTokenResponse>(tokenurl, {
       appId: String(appId),
       clientSecret: secret,
